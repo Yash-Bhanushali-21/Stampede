8107f0a878bb23903ac16ed609af3ac72d18fc42
yash11213018 <yash.bhanushali@sakec.ac.in>
2021-02-10 18:49:37 +0530

    final-version commit

---
+++ customer/pay_offline.php
@@ -0,0 +1,43 @@
+<center><!--  center Begin  -->
+    
+    <h1> Pay Offline Using Method </h1>
+    
+    <p class="text-muted">
+        
+        If you have any questions, feel free to <a href="../contact.php">Contact Us</a>. Our Customer Service work <strong>24/7</strong>
+        
+    </p>
+    
+</center><!--  center Finish  -->
+
+
+<hr>
+
+
+<div class="table-responsive"><!--  table-responsive Begin  -->
+    
+    <table class="table table-bordered table-hover table-striped"><!--  table table-bordered table-hover Begin  -->
+        
+        <thead><!--  thead Begin  -->
+            
+            <tr><!--  tr Begin  -->
+            
+                <th> Bank Account Details: </th>
+                <th> Ghatkopar East: </th>
+                <th> Western Union Details: </th>
+
+            </tr><!--  tr Finish  -->
+            
+        </thead><!--  thead Finish  -->
+        
+        <tbody><!--  tbody Begin  -->
+           
+           <td> Bank Name: JBL | Account No: 123-456-789 | Branch Name: Mumbai | Branch Code: 1111 </td>
+           <td> BID #111-111-111 | Mobile No: 7977992942 | Name: Yash Bhanushali </td>
+           <td> Real Name: Yash Bhanushali | Mobile No:7977992942 Country: India | NIC #980-231-907 </td>
+            
+        </tbody><!--  tbody Finish  -->
+        
+    </table><!--  table table-bordered table-hover Finish  -->
+    
+</div><!--  table-responsive Finish  -->
